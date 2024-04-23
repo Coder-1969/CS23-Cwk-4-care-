0c9d3d45fa3b2fb7a44f4c27376c49ed64930c8e
Alvinjv1 <167880606+Alvinjv1@users.noreply.github.com>
2024-04-23 16:42:45 +0100

    Champion.java

--- Champion.java
+++ Champion.java
@@ -1,18 +1,36 @@
 package cwk4;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Champion {
     private String championName;
     private int entryFee;
+    private String champType;
     private boolean isActive;
+    private boolean disqualified;
+    
 
-    public Champion(String name, int entryFee) {
+
+    public Champion(String name, String type,int entryFee) {
         this.championName = name;
+
+        if (type =="Warrior"||type=="Wizard"||type =="Dragon"){
+            champType = type;
+        }
+
         this.entryFee = entryFee;
         this.isActive = false;
     }
 
     public int getEntryFee() {
         return entryFee;
+    }
+    public boolean isDisqualified() {
+        return disqualified;
+    }
+    public void disqualify() {
+        disqualified = true; // Method to disqualify the champion
     }
 
     public void setActive(boolean active) {
@@ -21,6 +39,6 @@ public class Champion {
 
     @Override
     public String toString() {
-        return championName + (isActive ? " (active)" : "");
+        return championName + (isActive ? " (active)" : "")+ champType+(isDisqualified() ? " (disqualified)" : "") ;
     }
 }
