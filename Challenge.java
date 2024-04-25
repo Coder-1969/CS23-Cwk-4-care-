6bb5bb688b1a30a319082a5109cb2e09fcc0faec
Alvinjv1 <167880606+Alvinjv1@users.noreply.github.com>
2024-04-25 15:29:31 +0100

    Add files via upload

---
+++ Challenge.java
@@ -0,0 +1,37 @@
+package cwk4;
+
+class Challenge {
+    private int number;
+    private String opponentName;
+    private String type;
+    private int requiredSkillLevel;
+    private int reward;
+
+    public Challenge(int number, String enemyName, String type, int requiredSkillLevel, int reward) {
+        this.number = number;
+        this.opponentName = enemyName;
+        this.type = type;
+        this.requiredSkillLevel = requiredSkillLevel;
+        this.reward = reward;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getEnemyName() {
+        return opponentName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getRequiredSkillLevel() {
+        return requiredSkillLevel;
+    }
+
+    public int getReward() {
+        return reward;
+    }
+}
