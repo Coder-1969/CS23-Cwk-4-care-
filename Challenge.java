7a92506f81cde04267582ee62b7bb8bf0d23e60d
Alvinjv1 <167880606+Alvinjv1@users.noreply.github.com>
2024-04-25 17:54:24 +0100

    Challenge.java

--- Challenge.java
+++ Challenge.java
@@ -6,13 +6,15 @@ class Challenge {
     private String type;
     private int requiredSkillLevel;
     private int reward;
+    private int difficulityLevel;
 
-    public Challenge(int number, String enemyName, String type, int requiredSkillLevel, int reward) {
+    public Challenge(int number, String enemyName, String type, int requiredSkillLevel, int reward,int difficulityLevel) {
         this.number = number;
         this.opponentName = enemyName;
         this.type = type;
         this.requiredSkillLevel = requiredSkillLevel;
         this.reward = reward;
+        this.difficulityLevel = difficulityLevel;
     }
 
     public int getNumber() {
@@ -30,7 +32,9 @@ class Challenge {
     public int getRequiredSkillLevel() {
         return requiredSkillLevel;
     }
-
+    public int getDifficulityLevel(){
+        return difficulityLevel;
+    }
     public int getReward() {
         return reward;
     }
